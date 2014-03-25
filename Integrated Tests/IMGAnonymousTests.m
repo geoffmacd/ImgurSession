ac286646d0efae9ba03a7b60b7440485238557a3
Geoff MacDonald <gmacdonald@pivotallabs.com>
2014-03-25 15:46:59 -0400

    added anon tests to integrated and stubbed tests

--- Integrated Tests/IMGAnonymousTests.m
+++ Integrated Tests/IMGAnonymousTests.m
@@ -15,4 +15,132 @@
 
 @implementation IMGAnonymousTests
 
+- (void)testGalleryHot{
+    
+    __block BOOL isSuccess;
+    
+    [IMGGalleryRequest hotGalleryPage:0 success:^(NSArray * images) {
+        
+        isSuccess = YES;
+        
+    } failure:failBlock];
+    
+    expect(isSuccess).will.beTruthy();
+}
+
+- (void)testGalleryViral{
+    
+    __block BOOL isSuccess;
+    
+    [IMGGalleryRequest topGalleryPage:0 withWindow:IMGTopGalleryWindowDay withViralSort:YES success:^(NSArray * images) {
+        
+        isSuccess = YES;
+        
+    } failure:failBlock];
+    
+    expect(isSuccess).will.beTruthy();
+}
+
+- (void)testGalleryUser{
+    
+    __block BOOL isSuccess;
+    
+    [IMGGalleryRequest userGalleryPage:0 withViralSort:YES showViral:YES success:^(NSArray * images) {
+        
+        isSuccess = YES;
+        
+    } failure:failBlock];
+    
+    expect(isSuccess).will.beTruthy();
+}
+
+- (void)testGalleryImage{
+    
+    __block BOOL isSuccess;
+    
+    [IMGGalleryRequest imageWithID:@"mOqejNf" success:^(IMGGalleryImage *image) {
+        
+        expect(image.imageID).beTruthy();
+        isSuccess = YES;
+        
+    } failure:failBlock];
+    
+    expect(isSuccess).will.beTruthy();
+}
+
+- (void)testImage{
+    
+    __block BOOL isSuccess;
+    
+    [IMGImageRequest imageWithID:@"mOqejNf" success:^(IMGImage *image) {
+        
+        expect(image.imageID).beTruthy();
+        isSuccess = YES;
+        
+    } failure:failBlock];
+    
+    expect(isSuccess).will.beTruthy();
+}
+
+
+- (void)testGalleryAlbum{
+    
+    __block BOOL isSuccess;
+    
+    [IMGGalleryRequest albumWithID:@"HtAOg" success:^(IMGGalleryAlbum *album) {
+        
+        expect(album.albumID).beTruthy();
+        isSuccess = YES;
+        
+    } failure:failBlock];
+    
+    expect(isSuccess).will.beTruthy();
+}
+
+- (void)testAlbum{
+    
+    __block BOOL isSuccess;
+    
+    [IMGAlbumRequest albumWithID:@"HtAOg" success:^(IMGAlbum *album) {
+        
+        expect(album.albumID).beTruthy();
+        isSuccess = YES;
+        
+    } failure:failBlock];
+    
+    expect(isSuccess).will.beTruthy();
+}
+
+- (void)testUsersAccount{
+    
+    __block IMGAccount * acc;
+    
+    [IMGAccountRequest accountWithUser:imgurUnitTestParams[@"recipientId"]  success:^(IMGAccount *account) {
+        
+        expect(account.bio).beTruthy();
+        expect(account.accountID).beTruthy();
+        expect(account.username).beTruthy();
+        acc = account;
+        
+    } failure:failBlock];
+    
+    expect(acc).willNot.beNil();
+}
+
+- (void)testUsersComments{
+    
+    __block BOOL isSuccess;
+    
+    [IMGAccountRequest accountCommentsWithUser:imgurUnitTestParams[@"recipientId"] success:^(NSArray * comments) {
+        
+        expect(comments).haveCountOf(1);
+        IMGComment * first = [comments firstObject];
+        expect(first.commentId).beTruthy();
+        isSuccess = YES;
+        
+    } failure:failBlock];
+    
+    expect(isSuccess).will.beTruthy();
+}
+
 @end
