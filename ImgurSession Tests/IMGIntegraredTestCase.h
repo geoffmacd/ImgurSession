a9243093e62962449b8ad40d0515f87c25003928
Geoff MacDonald <gmacdonald@pivotallabs.com>
2014-03-25 14:17:54 -0400

    added targets integrated tests with new superclass test case

---
+++ ImgurSession Tests/IMGIntegraredTestCase.h
@@ -0,0 +1,13 @@
+//
+//  IMGIntegraredTestCase.h
+//  ImgurSession
+//
+//  Created by Xtreme Dev on 2014-03-25.
+//  Copyright (c) 2014 GeoffMacDonald. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IMGIntegraredTestCase : XCTest
+
+@end
---
+++ ImgurSession Tests/IMGIntegraredTestCase.m
@@ -0,0 +1,13 @@
+//
+//  IMGIntegraredTestCase.m
+//  ImgurSession
+//
+//  Created by Xtreme Dev on 2014-03-25.
+//  Copyright (c) 2014 GeoffMacDonald. All rights reserved.
+//
+
+#import "IMGIntegraredTestCase.h"
+
+@implementation IMGIntegraredTestCase
+
+@end
